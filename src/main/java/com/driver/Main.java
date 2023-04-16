c7eaa552c7669b8e01f44aab30b8691890664796
mozammilanwar <mozammilanwar66@gmail.com>
2023-04-17 04:27:37 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //   obj.name = "Shubham";
+        //   System.out.println(obj.name);
+        obj.setName("Mozammil");
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
 
+/*
+ *
+ * Exception in thread "main" java.lang.Error: Unresolved compilation problems:
+        The field RWOnly.name is not visible
+        The field RWOnly.name is not visible
+        at com.driver.Main.main(Main.java:6)
+ *
+*/
 public class RWOnly {
-    
+    private String name;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
 }
